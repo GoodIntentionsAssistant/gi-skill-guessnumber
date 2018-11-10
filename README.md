79c7e4e175d436aae82149c1bfb371f09ef509aa
Darren Moore <darren.m@firecreekweb.com>
2018-11-10 21:13:05 +0700

    Moved logging to debug

---
+++ README.md
@@ -0,0 +1,7 @@
+# Good Intentions Skill Guess Number game
+
+Guess number game skill for Good Intentions Assistant
+
+## Documentation
+
+http://goodintentions.firecreekweb.com
---
+++ guessnumber_skill.js
@@ -0,0 +1,19 @@
+/**
+ * Guess number Skill
+ */
+var Skill = require('../../../src/Skill/skill');
+
+module.exports = class GuessnumberSkill extends Skill {
+
+/**
+ * Constructor
+ *
+ * @param App app
+ * @access public
+ * @return void
+ */
+	constructor(app) {
+		super(app);
+	}
+
+}
